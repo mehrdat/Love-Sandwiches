5d8ee9cd7a6ce0a3262edb37cb25f0dbc898116e
Mehrdad Lashgari <mehr.lashgari@gmail.com>
2022-02-08 10:39:21 +0000

    def get sales data function

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -10,11 +7,26 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS=Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS=CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT=gspread.authorize(SCOPED_CREDS)
-SHEET=GSPREAD_CLIENT.open('love-sandwiches')
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales=SHEET.worksheet('sales')
-data=sales.get_all_values()
-print(data)
+sales = SHEET.worksheet('sales')
+
+#data = sales.get_all_values()
+
+#print(data)
+
+def get_sales_data():
+    """
+    get sales data from user"""
+    print('please enter the sales data from your last market')
+    print('data should be six numbers spread with comas')
+    print('Example: 32,45,32,14,70')
+
+    data_str=input('Data here:')
+    print(f'the data provided is {data_str}')
+
+
+get_sales_data()
