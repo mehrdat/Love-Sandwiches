96c6439119dcf6d9040ee72f559e592e7e3b5cb2
Mehrdad Lashgari <mehr.lashgari@gmail.com>
2022-02-08 15:51:01 +0000

    surplus function

--- run.py
+++ run.py
@@ -63,8 +63,14 @@ def calculate_surplus_data(sales_row):
     stock=SHEET.worksheet('stock').get_all_values()
     #pprint(stock)
     stock_row=stock[-1]
-    print(stock_row)
-
+    
+    surplus_data=[]
+    for stock,sales in zip(stock_row,sales_row):
+        surplus=int(stock)-sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
+    
 
 def main():
     """
