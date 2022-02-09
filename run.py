739290ed20ba5033c5a5558a34d8be20ee08f2d6
Mehrdad Lashgari <mehr.lashgari@gmail.com>
2022-02-09 14:12:38 +0000

    finished the exam

--- run.py
+++ run.py
@@ -132,7 +132,23 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    
+    return stock_values
 
 
-print("Welcome to Love Sandwiches Data Automation")
-main()
+def get_stock_values(data):
+    worksheet_keys=SHEET.worksheet('sales')
+    #print(worksheet_keys.col_values(4)[0])
+    keys=[]
+    for num in range(5):
+        keys[num]=worksheet_keys.col_values(num)[0]
+    dict={}
+    for ky,num in zip(keys,range(5):
+        [dict[key]=data[num]
+    pprint(dict)
+
+#stock_values=get_stock_values(stock_data)
+
+#print("Welcome to Love Sandwiches Data Automation")
+#stock_data=main()
+get_stock_values()
